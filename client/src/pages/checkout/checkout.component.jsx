65f877a34514d3fc804c4f463b65109f8095d310
bipendra <bipendra.kumar@gaana.com>
2020-10-05 02:38:39 +0530

    changed structure and directory location

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import CheckoutItem from '../../../components/checkout-item/checkout-item.component';
-import StripeCheckoutButton from '../../../components/stripe-button/stripe-button.component';
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import {
   selectCartItems,
   selectCartTotal
-} from '../../../redux/cart/cart.selectors';
+} from '../../redux/cart/cart.selectors';
 
 import {
   CheckoutPageContainer,
--- client/src/pages/collection/collection.container.jsx
+++ client/src/pages/collection/collection.container.jsx
@@ -2,8 +2,8 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { createStructuredSelector } from 'reselect';
 
-import { selectIsCollectionsLoaded } from '../../../redux/shop/shop.selectors';
-import WithSpinner from '../../../components/with-spinner/with-spinner.component';
+import { selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 import CollectionPage from './collection.component';
 
 const mapStateToProps = createStructuredSelector({
